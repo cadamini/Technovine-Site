873a4283a91b4af7ca6703ad79c268a06482b40b
Richard Joubert <rjoubert@nsd.team>
2023-02-10 22:07:42 -0500

    node update
    Signed-off-by: Richard Joubert <rjoubert@nsd.team>

--- _drafts/html/compress-regex.js
+++ _drafts/html/compress-regex.js
@@ -1,6 +1,7 @@
 const replace = require('replace-in-file');
+const file = require('file-system');
 const options = {
-  files: './compress.txt',
+  files: file.readFile("./html-compress.txt"),
   from: /(\t|\s{2,}|\n)/gm,
   to: 'bar',
 };
