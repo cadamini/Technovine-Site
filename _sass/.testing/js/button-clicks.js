1fefb64176d101e5c1b9a74a947d7fd235e8ae6f
PatriotsForLife <mayowaogunbowale@gmail.com>
2023-03-26 00:13:58 -0500

    dropwdown,header,sales widget
    Signed-off-by: PatriotsForLife <mayowaogunbowale@gmail.com>

---
+++ _sass/.testing/js/button-clicks.js
@@ -0,0 +1,11 @@
+
+
+
+
+function requestService() {
+	document.GetElementById("___").addEventListener("click", function() {
+		document.GetElementById("service-request_button").setAttribute("class", "PRESSED");
+		// delay of 200ms
+		style.ORIGINALCLASS
+	});
+}
